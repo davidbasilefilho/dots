b0cfc9e7b47f0bbb5a974314690c1fcc56064d01
David Basile Filho <davidbasilefilho@gmail.com>
2025-01-13 14:54:56 -0300

    test

--- nvim/lua/custom/plugins/zen-mode.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'folke/zen-mode.nvim',
-  config = function()
-    local zenmode = require 'zen-mode'
-    zenmode.setup {}
-
-    vim.keymap.set('n', '<leader>tz', '<cmd>ZenMode<CR>', { desc = '[T]oggle Zen Mode' })
-  end,
-}
